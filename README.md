cc3ebec8ae5d7ca09d9c3dbbfc2aaf37e1e14d32
Os <os@os.com>
2021-11-09 06:13:03 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# proxy_checker
+
+## Table of contents
+
+* [Introduction](#introduction)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Alternatives](#alternatives)
+
+## Introduction
+
+**proxy_checker** is a simple Python script that takes a file with a proxy on each line in the format:
+
+~~~
+socks5://128.199.111.243:14959
+https://128.199.111.243:443
+http://128.199.111.243:80
+~~~
+
+and quickly verifies which are up and running by proxying requests to [HTTPBin](httpbin.org/get) and checking to see if:
+
+* the connection was successful
+* your original IP was forwarded or not
+
+It then saves working proxies that do not leak your IP to a file of your choice in addition to displaying them on stdout.
+
+## Installation
+
+The only pre-requisite for this tool are Python 3 and the **requests[socks]** package. To install it with its pre-requisites:
+
+~~~
+git clone ...
+cd proxy_checker
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
+~~~
+
+## Usage
+
+Usage is straight-forward. If you do not explicitly provide a input and output file, it will default to **proxies.txt** and **live.txt**:
+
+~~~
+Usage: proxy_checker [-h] [-i INPUT] [-o OUTPUT]
+
+Verify that list of proxies are working
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        input file
+  -o OUTPUT, --output OUTPUT
+                        output file
+~~~
+
+By default, it queries 25 proxies concurrently. This is configurable via the **THREADS** constant. It waits for up to 20 seconds for a proxy to reply. This too is configurable via the **TIMEOUT** variable.
+
+## Alternatives
+
+For a quick one-liner alternative, you can use **curl**:
+
+~~~
+curl -x socks5h://localhost:8001 http://www.httpbin.org/get
+curl -x https://localhost:8001 http://www.httpbin.org/get
+curl -x http://localhost:8001 http://www.httpbin.org/get
+~~~
+
+There are also a number of other tools that do something similar. A GitHub search will point you in the right direction.
---
+++ proxy_checker.py
@@ -0,0 +1,89 @@
+"""
+proxy_checker is a simple Python script that takes a file with a proxy on each line in the format:
+
+socks5://128.199.111.243:14959
+https://128.199.111.243:443
+http://128.199.111.243:80
+
+and quickly verifies which are up and running by proxying requests to https://httpbin.org/get and checking to see if:
+
+* the connection was successful
+* your original IP was forwarded or not
+
+It then saves working proxies that do not leak your IP to a file of your choice in addition to displaying them on stdout.
+"""
+
+import argparse
+import json
+import requests
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+
+TIMEOUT = 20
+THREADS = 25
+INPUT = "proxies.txt"
+OUTPUT = "live.txt"
+URL = "http://httpbin.org/get"
+live = []
+
+
+def fetch_real_ip():
+    """
+    uses http://httpbin.or/get to get our real IP and returns it
+    """
+    try:
+        resp = requests.get(URL, timeout=TIMEOUT).text
+        return json.loads(resp)["origin"]
+    except:
+        pass
+
+
+def check_proxy(proxy, real_ip):
+    """
+    checks a proxy by sending to URL and checking that our real IP is not
+    included in the response. Only then is it returned as a valid proxy
+
+    :param proxy: the proxy we want to test. This is in the format:
+    socks5://127.0.0.1
+    :param real_ip: our real IP addres
+    """
+    proxies = {"http": proxy}
+    try:
+        resp = requests.get(URL, proxies=proxies, timeout=TIMEOUT)
+        if resp.status_code == 200:
+            if real_ip not in resp.text:
+                print(proxy)
+                live.append(proxy)
+    # except requests.exceptions.ConnectTimeout:
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    cli_parser = argparse.ArgumentParser(
+        description="Check that proxies in list are working", prog="proxy_checker"
+    )
+    cli_parser.add_argument("-i", "--input", help="input file")
+    cli_parser.add_argument("-o", "--output", help="output file")
+    cli_options = cli_parser.parse_args()
+
+    if cli_options.input:
+        INPUT = cli_options.input
+
+    if cli_options.output:
+        OUTPUT = cli_options.output
+
+    # get your real IP to check if it is leaked by the proxy
+    real_ip = fetch_real_ip()
+
+    with open(INPUT) as file:
+        lines = file.readlines()
+        proxy_list = [line.rstrip() for line in lines]
+
+    with ThreadPoolExecutor(THREADS) as pool:
+        for i in proxy_list:
+            pool.submit(check_proxy, i, real_ip)
+
+    with open(OUTPUT, "w") as f:
+        for i in live:
+            f.write("%s\n" % i)
